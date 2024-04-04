30c1c44b1dccc464c7a8ec4fbb423ab1ae0606f4
ofzlo <yourmejo@gmail.com>
2024-04-04 13:28:04 +0000

    refactor: split parse_ars() and main part

--- test.py
+++ test.py
@@ -7,31 +7,18 @@ from torch.utils.data import DataLoader
 
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--we_path', default='GoogleNews-vectors-negative300.bin', type=str)
-parser.add_argument('--data_path', default='msrvtt_category_test.pkl', type=str)
-parser.add_argument('--checkpoint_path', default='epoch210.pth', type=str)
-parser.add_argument('--token_projection', default='projection_net', type=str) 
-parser.add_argument('--use_softmax', default=False, type=bool) 
-parser.add_argument('--use_cls_token', default=False, type=bool) 
-parser.add_argument('--num_classes', default=20, type=int) 
-parser.add_argument('--batch_size', default=16, type=int) 
-args = parser.parse_args()
-
-checkpoint = torch.load(args.checkpoint_path)
-
-we = None 
-we = KeyedVectors.load_word2vec_format(args.we_path, binary=True)
-
-dataset = MSRVTT_DataLoader(data_path=args.data_path, we=we)
-data_loader = DataLoader(dataset, batch_size=args.batch_size)
-
-net = EverythingAtOnceModel(args).cuda()
-optimizer = torch.optim.Adam(net.parameters(), lr =0.001)
-
-net.load_state_dict(checkpoint['model_state_dict'])
-optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-net.eval()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--we_path', default='GoogleNews-vectors-negative300.bin', type=str)
+    parser.add_argument('--data_path', default='msrvtt_category_test.pkl', type=str)
+    parser.add_argument('--checkpoint_path', default='epoch210.pth', type=str)
+    parser.add_argument('--token_projection', default='projection_net', type=str) 
+    parser.add_argument('--use_softmax', default=False, type=bool) 
+    parser.add_argument('--use_cls_token', default=False, type=bool) 
+    parser.add_argument('--num_classes', default=20, type=int) 
+    parser.add_argument('--batch_size', default=16, type=int) 
+    args = parser.parse_args()
+    return args
 
 def get_soft_voting(va, at, tv):
     # Soft voting by averaging the probabilities
@@ -57,33 +44,50 @@ def calculate_accuracy(predictions, labels):
     accuracy = correct / total
     return accuracy
 
-total_samples = 0
-total_accuracy = 0
-total_video_correct = 0
-total_audio_correct = 0
-total_text_correct = 0
-total_hard_vote_correct = 0
-total_soft_vote_correct = 0
+if __name__ == '__main__':
+    args = parse_args()
 
-for data in data_loader:
-    video = data['video'].cuda()
-    audio = data['audio'].cuda()
-    text = data['text'].cuda()
-    nframes = data['nframes'].cuda()
-    category = data['category'].cuda() # [batch_size,]
+    checkpoint = torch.load(args.checkpoint_path)
 
-    video = video.view(-1, video.shape[-1])
-    audio = audio.view(-1, audio.shape[-2], audio.shape[-1])
-    text = text.view(-1, text.shape[-2], text.shape[-1])
+    we = None 
+    we = KeyedVectors.load_word2vec_format(args.we_path, binary=True)
 
-    pred = net(video, audio, nframes, text, category) # [batch_size, 20]
-    pred_category = torch.argmax(pred, dim=1) # [batch_size,]
-    accuracy = torch.mean((pred_category == category).float()) # [batch_size,]
-    print(pred_category, '/', category)
+    dataset = MSRVTT_DataLoader(data_path=args.data_path, we=we)
+    data_loader = DataLoader(dataset, batch_size=args.batch_size)
 
-    total_accuracy += accuracy
+    net = EverythingAtOnceModel(args).cuda()
+    optimizer = torch.optim.Adam(net.parameters(), lr =0.001)
 
-# Calculate final accuracies
-accuracy = total_accuracy / len(data_loader)
+    net.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    net.eval()
 
-print("Accuracy:", accuracy)
+    total_samples = 0
+    total_accuracy = 0
+    total_video_correct = 0
+    total_audio_correct = 0
+    total_text_correct = 0
+    total_hard_vote_correct = 0
+    total_soft_vote_correct = 0
+
+    for data in data_loader:
+        video = data['video'].cuda()
+        audio = data['audio'].cuda()
+        text = data['text'].cuda()
+        nframes = data['nframes'].cuda()
+        category = data['category'].cuda() # [batch_size,]
+
+        video = video.view(-1, video.shape[-1])
+        audio = audio.view(-1, audio.shape[-2], audio.shape[-1])
+        text = text.view(-1, text.shape[-2], text.shape[-1])
+
+        pred = net(video, audio, nframes, text, category) # [batch_size, 20]
+        pred_category = torch.argmax(pred, dim=1) # [batch_size,]
+        accuracy = torch.mean((pred_category == category).float()) # [batch_size,]
+        print(pred_category, '/', category)
+
+        total_accuracy += accuracy
+
+    # Calculate final accuracies
+    accuracy = total_accuracy / len(data_loader)
+    print("Accuracy:", accuracy)
