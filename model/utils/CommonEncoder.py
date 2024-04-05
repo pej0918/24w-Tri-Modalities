57ec187b7edf8882d7d05ced53184a73eee4eedf
ofzlo <yourmejo@gmail.com>
2024-04-05 07:34:33 +0000

    add: CommonEncoder

---
+++ model/utils/CommonEncoder.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch 
+import torch.nn.functional as F
+
+
+class CommonEncoder(nn.Module):
+
+    def __init__(self, common_dim, latent_dim):
+        super(CommonEncoder, self).__init__()
+        self.common_dim = common_dim
+        self.latent_dim = latent_dim
+
+        self.feature_extractor = nn.Sequential(
+            nn.Linear(common_dim, 512),
+            Swish(),
+            nn.Linear(512, 512),
+            Swish(),
+            nn.Linear(512, latent_dim),
+        )
+
+    def forward(self, x):
+        return F.normalize(self.feature_extractor(x), dim=-1)
+
+
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
